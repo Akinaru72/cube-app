2c2d8d9e994cbb0783849fbc29e008b686b4724b
Volodymyr <akinaru3@gmail.com>
2026-07-09 17:01:00 +0300

    add scramble

---
+++ src/js/constants.js
@@ -0,0 +1,29 @@
+// constants.js;
+export const COLORS = {
+  right: 0xff0000,
+  left: 0xffa500,
+  top: 0xffffff,
+  bottom: 0xffff00,
+  front: 0x00ff00,
+  back: 0x0000ff,
+  inner: 0x111111,
+};
+
+export const MOVES = [
+  'R',
+  'RPrime',
+  'L',
+  'LPrime',
+  'U',
+  'UPrime',
+  'D',
+  'DPrime',
+  'F',
+  'FPrime',
+  'B',
+  'BPrime',
+];
+
+export const CUBE_SIZE = 0.95;
+
+export const ROTATION_SPEED = 0.05;
---
+++ src/js/controls.js
@@ -0,0 +1,27 @@
+// controls.js
+export function initKeyboard(cube) {
+  console.log('Hello');
+  const KEY_MAP = {
+    r: () => cube.R(),
+    R: () => cube.RPrime(),
+
+    l: () => cube.L(),
+    L: () => cube.LPrime(),
+
+    u: () => cube.U(),
+    U: () => cube.UPrime(),
+
+    d: () => cube.D(),
+    D: () => cube.DPrime(),
+
+    f: () => cube.F(),
+    F: () => cube.FPrime(),
+
+    b: () => cube.B(),
+    B: () => cube.BPrime(),
+  };
+
+  document.addEventListener('keydown', event => {
+    KEY_MAP[event.key]?.();
+  });
+}
